bf2515013e1e2259a83571f74742c3d99d738b9f
iamdenzen <39949175+iamdenzen@users.noreply.github.com>
2026-07-03 20:09:08 +0500

    Disabled unused scopes

--- includes/Auth/OAuth/ScopeRegistry.php
+++ includes/Auth/OAuth/ScopeRegistry.php
@@ -10,22 +10,22 @@ class ScopeRegistry {
 	public static function all(): array {
 		$scopes = [
 			'wp:read'            => 'Read general WordPress data.',
-			'wp:write'           => 'Write general WordPress data.',
+			//'wp:write'           => 'Write general WordPress data.',
 
 			'posts:read'         => 'Read posts.',
-			'posts:write'        => 'Create or update posts.',
+			//'posts:write'        => 'Create or update posts.',
 
 			'pages:read'         => 'Read pages.',
-			'pages:write'        => 'Create or update pages.',
+			//'pages:write'        => 'Create or update pages.',
 
 			'media:read'         => 'Read media library items.',
-			'media:write'        => 'Upload or update media.',
+			//'media:write'        => 'Upload or update media.',
 
 			'woocommerce:read'   => 'Read WooCommerce data.',
-			'woocommerce:write'  => 'Create or update WooCommerce data.',
+			//'woocommerce:write'  => 'Create or update WooCommerce data.',
 
-			'acf:read'           => 'Read ACF fields.',
-			'acf:write'          => 'Write ACF fields.',
+			//'acf:read'           => 'Read ACF fields.',
+			//'acf:write'          => 'Write ACF fields.',
 		];
 
 		return apply_filters( 'wp_mcp_server_oauth_scopes', $scopes );
